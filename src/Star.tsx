f1bae3b48e7a1c6b88d0c354d43b10dcbd33072d
Eric Constantinides <eric@ericconstantinides.com>
2020-10-28 17:50:35 -0700

    remove content

--- src/Star.tsx
+++ src/Star.tsx
@@ -6,14 +6,8 @@ import { getRandomInt } from './utilities/utilities'
 const cj = cssJoinFactory(styles)
 
 const Star: React.FC = () => {
-  const bottom = getRandomInt(1, 1000)
-  const left = getRandomInt(1, 1000)
-  const style = {
-    bottom: (bottom * .1) + '%',
-    left: (left * .1) + '%',
-  }
   return (
-    <div className={cj('star')} style={style} />
+    <div>&nbsp;</div>
   )
 }
 
--- src/Stars.tsx
+++ src/Stars.tsx
@@ -4,13 +4,9 @@ import Star from './Star'
 const NUM_STARS = 500
 
 const Stars = () => {
-  let stars = []
-  for (let i = 0; i < NUM_STARS; i++) {
-    stars.push(<Star key={i} />)
-  }
   return (
     <div className="stars">
-      {stars}
+      {/* {stars} */}
     </div>
   )
 }
